4f3c973ea7c138dcfc5cfb75b73c7ecca5d9612d
junep16 <jiyunpark0816@gmail.com>
2022-02-16 22:17:29 +0900

    💄 할일목록 스타일링

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import "./App.css";
 
 
 export default class App extends Component {
+  btnStyle = {
+    color: "#FFF", 
+    border: "none", 
+    padding: "5px 9px", 
+    borderRadius: "50%", 
+    cursor: "pointer", 
+    float: "right"
+  }
+
+  getStyle = () => {
+    return {
+      padding: "10px", 
+      borderBottom: "1px #CCC dotted", 
+      textDecoration: "none"
+    }
+  }
+
   render() {
     return (
       <div className="container">
         <div className="todoBlock">
           <div className="title">
             <h1>할 일 목록</h1>
+          </div>
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false}></input>
+            공부하기 
+            <button style={this.btnStyle}>X</button>
           </div>
         </div>
       </div>
