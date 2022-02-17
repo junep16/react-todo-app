323ee158cf0197026f68b8fc9e43f2f557c2da19
junep16 <jiyunpark0816@gmail.com>
2022-02-17 21:34:12 +0900

    ✨ 삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import "./App.css";
 
 
 export default class App extends Component {
+
+  state = {
+    todoData : [
+      {
+        id: "1", 
+        title: "공부하기", 
+        completed: true, 
+      }, 
+      {
+        id: "2", 
+        title: "청소하기", 
+        completed: true, 
+      }, 
+      {
+        id: "3", 
+        title: "쇼핑하기", 
+        completed: false, 
+      }, 
+    ]
+  }
+
+
   btnStyle = {
     color: "#FFF", 
     border: "none", 
@@ -20,23 +42,11 @@ export default class App extends Component {
     }
   }
 
-  todoData = [
-    {
-      id: "1", 
-      title: "공부하기", 
-      completed: true, 
-    }, 
-    {
-      id: "2", 
-      title: "청소하기", 
-      completed: true, 
-    }, 
-    {
-      id: "3", 
-      title: "쇼핑하기", 
-      completed: false, 
-    }, 
-  ]
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id)
+    console.log("newTodoData", newTodoData); 
+    this.setState({todoData: newTodoData}); 
+  };
 
   render() {
     return (
@@ -45,11 +55,11 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
-          {this.todoData.map((data) => (
+          {this.state.todoData.map((data) => (
             <div style={this.getStyle()} key={data.id}>
               <input type="checkbox" defaultChecked={false}></input>
               {data.title} 
-              <button style={this.btnStyle}>X</button>
+              <button style={this.btnStyle} onClick= {() => this.handleClick(data.id)}>X</button>
             </div> 
           ))}
         </div>
