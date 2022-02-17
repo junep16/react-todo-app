b2286eef4c338cb3cfbcd69af202b043e373357e
junep16 <jiyunpark0816@gmail.com>
2022-02-17 22:00:17 +0900

    ✨ 인풋박스 추가 및 기능 구현

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ export default class App extends Component {
         title: "쇼핑하기", 
         completed: false, 
       }, 
-    ]
+    ],
+    value: ""
   }
 
 
@@ -48,12 +49,30 @@ export default class App extends Component {
     this.setState({todoData: newTodoData}); 
   };
 
+  handleChange = (e) => {  
+    this.setState({value : e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault(); 
+
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(), 
+      title: this.state.value, 
+      completed: false, 
+    }
+
+    // 원래 있던 할 일에 새로운 할 일 더해주기 
+    this.setState({ todoData: [...this.state.todoData, newTodo]}); 
+  }
+
   render() {
     return (
       <div className="container">
         <div className="todoBlock">
           <div className="title">
-            <h1>할 일 목록</h1>
+            <h1>TO DO</h1>
           </div>
           {this.state.todoData.map((data) => (
             <div style={this.getStyle()} key={data.id}>
@@ -62,6 +81,22 @@ export default class App extends Component {
               <button style={this.btnStyle} onClick= {() => this.handleClick(data.id)}>X</button>
             </div> 
           ))}
+          <form style={{ display: "flex"}} onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="value"
+              style={{flex: "10", padding: "5px"}}
+              placeholder="해야할 일을 입력해주세요"
+              value={this.state.value}
+              onChange={this.handleChange}
+            /> 
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{flex:1}}
+            />
+          </form>
         </div>
       </div>
     )
