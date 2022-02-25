c6cb49e7a40ec8abad4fa1636d5815ea1cec1954
junep16 <jiyunpark0816@gmail.com>
2022-02-25 23:05:15 +0900

    ✅ hooks 사용으로 업데이트

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import "./App.css"; 
 
 export default function App() {
-  
+
   const [todoData, setTodoData] = useState([]); 
   const [value, setValue] = useState(""); 
 
 
-  btnStyle = {
+  const btnStyle = {
     color: "#FFF", 
     border: "none", 
     padding: "5px 9px", 
@@ -16,7 +16,7 @@ export default function App() {
     float: "right"
   }
 
-  getStyle = (completed) => {
+  const getStyle = (completed) => {
     return {
       padding: "10px", 
       borderBottom: "1px #CCC dotted", 
@@ -24,66 +24,64 @@ export default function App() {
     }
   }
 
-  handleClick = (id) => {
-    let newTodoData = this.state.todoData.filter((data) => data.id !== id)
-    console.log("newTodoData", newTodoData); 
-    this.setState({todoData: newTodoData}); 
+  const handleClick = (id) => {
+    let newTodoData = todoData.filter((data) => data.id !== id)
+    console.log("newTodoData", newTodoData);  
+    setTodoData(newTodoData)
   };
 
-  handleChange = (e) => {  
-    this.setState({value : e.target.value})
+  const handleChange = (e) => {  
+    setValue(e.target.value); 
   }
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault(); 
 
     // 새로운 할 일 데이터
     let newTodo = {
       id: Date.now(), 
-      title: this.state.value, 
+      title: value, 
       completed: false, 
     }
 
     // 원래 있던 할 일에 새로운 할 일 더해주기 
-    this.setState({ todoData: [...this.state.todoData, newTodo], value: ""}); 
+    setTodoData(prev => [...prev, newTodo]); 
+    setValue(""); 
   }
   
-  handleCompleteChange = (id) => {
-    let newTodoData = this.state.todoData.map(data => {
+  const handleCompleteChange = (id) => {
+    let newTodoData = todoData.map(data => {
       if(data.id === id) {
         data.completed = !data.completed; 
       }
       return data; 
     })
-
-    this.setState({ todoData: newTodoData}); 
+    setTodoData(newTodoData);  
     // 로컬에 저장 
     // window.localStorage.setItem("newTodoData", JSON.stringify(newTodoData)); 
   }
   
-
-{
     return (
       <div className="container">
         <div className="todoBlock">
           <div className="title">
             <h1>✅ TO DO</h1>
           </div>
-          {this.state.todoData.map((data) => (
-            <div style={this.getStyle(data.completed)} key={data.id}>
-              <input type="checkbox" defaultChecked={false} onChange={() => { this.handleCompleteChange(data.id)}}></input>
+          {todoData.map((data) => (
+            <div style={getStyle(data.completed)} key={data.id}>
+              <input type="checkbox" defaultChecked={false} onChange={() => {handleCompleteChange(data.id)}}></input>
               {data.title} 
-              <button style={this.btnStyle} onClick= {() => this.handleClick(data.id)}>X</button>
+              <button style={btnStyle} onClick= {() => handleClick(data.id)}>X</button>
             </div> 
           ))}
-          <form style={{ display: "flex"}} onSubmit={this.handleSubmit}>
+          <form style={{ display: "flex"}} onSubmit={handleSubmit}>
             <input
               type="text"
               name="value"
               style={{flex: "10", padding: "5px"}}
               placeholder="해야할 일을 입력해주세요"
-              value={this.state.value}
-              onChange={this.handleChange}
+              value={value}
+              onChange={handleChange}
             /> 
             <input
               type="submit"
@@ -96,4 +94,3 @@ export default function App() {
       </div>
     )
   }
-}
